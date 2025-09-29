0b77764d6a6ab326284d6efcf4dc25df3e699bf2
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-09-30 00:18:53 +0500

    feat: add theme toggle functionality and navbar component
    - Integrated `next-themes` for theme management.
    - Created a `Navbar` component with a logo and global search placeholder.
    - Implemented `ModeToggle` component for switching between light, dark, and system themes using a dropdown menu.
    - Updated global styles with new CSS variables for theming.
    - Refactored layout to include `ThemeProvider` and `Navbar`.
    - Added Radix UI dropdown menu for theme selection.
    - Updated dependencies in `package.json` for new components.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next';
 import { Inter, Space_Grotesk } from 'next/font/google';
+
 import './globals.css';
+import Navbar from '@/components/navigation/navbar';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const inter = Inter({
   variable: '--font-inter',
@@ -27,8 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${inter.className} ${spaceGrotesk.variable} antialiased`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/navigation/navbar.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { ModeToggle } from '../shared/mode-toggle';
+
+const Navbar = () => {
+  return (
+    <nav className="fixed z-50 flex-between w-full gap-5 background-light900_dark200 p-6 shadow-light-300 sm:px-12 dark:shadow-none">
+      <Link href={'/'} className="flex items-center gap-1">
+        <Image src="/images/site-logo.svg" alt="CodeFlow Logo" width={23} height={23} />
+        <p className="font-space-grotesk h2-bold text-dark100_light900 max-sm:hidden">
+          Code <span className="text-primary-500">Flow</span>
+        </p>
+      </Link>
+
+      <p>Global Search</p>
+
+      <div className="flex-between gap-5">
+        <ModeToggle />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
