801bb23f68318f0fb039170799b8e0d994dbd1f3
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-01 19:26:46 +0500

    feat: create user model and interface for MongoDB integration

---
+++ src/database/user.model.ts
@@ -0,0 +1,19 @@
+import { model, models, Schema } from 'mongoose';
+
+const UserSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    bio: { type: String },
+    image: { type: String, required: true },
+    location: { type: String },
+    portfolio: { type: String },
+    reputation: { type: Number, default: 0 },
+  },
+  { timestamps: true },
+);
+
+const User = models?.User || model<IUser>('User', UserSchema);
+
+export default User;
---
+++ src/types/model.d.ts
@@ -0,0 +1,10 @@
+interface IUser {
+  name: string;
+  username: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
