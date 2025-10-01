a71c9136beb9f7caf31fb4b0ca4de6c0a82c2299
solijoniyjahongirmirzo <jahongirsolijoniy@gmail.com>
2025-10-01 19:11:13 +0500

    feat: add mongoose integration for MongoDB connection handling

---
+++ src/lib/mongoose.ts
@@ -0,0 +1,47 @@
+import mongoose, { Mongoose } from 'mongoose';
+
+const MONGO_URI = process.env.MONGODB_URI as string;
+
+if (!MONGO_URI) {
+  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+}
+
+interface MongooseCache {
+  conn: Mongoose | null;
+  promise: Promise<Mongoose> | null;
+}
+
+declare global {
+  var mongoose: MongooseCache;
+}
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const dbConnect = async (): Promise<Mongoose> => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(MONGO_URI, { dbName: 'codeflow' })
+      .then((mongoose) => {
+        console.log('Connected to MongoDB');
+        return mongoose;
+      })
+      .catch((error) => {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+      });
+  }
+
+  cached.conn = await cached.promise;
+
+  return cached.conn;
+};
+
+export default dbConnect;
